532cf516b7e6caf0eebceb031f1696be9462112f
Yomei Otani <yomei.otani@gmail.com>
2022-02-14 12:37:16 +0900

    Add files via upload

---
+++ server/scatter_plot/build.sh
@@ -0,0 +1,23 @@
+CSRCDIR=src
+JSRCDIR=out
+TOOLDIR=tool
+HTMLDIR=html
+
+FILES="scatter_plot"
+
+mkdir -p $JSRCDIR
+
+for i in $FILES; do
+echo $i
+g++ -o $JSRCDIR/$i.js -DJAVASCRIPT -E -C -P $CSRCDIR/$i.cpp
+done
+
+cp -f $HTMLDIR/main.html $JSRCDIR/.
+cp -f $HTMLDIR/jquery*.js $JSRCDIR/.
+cp -f $HTMLDIR/chart*.js $JSRCDIR/.
+
+
+echo ------------------------------
+echo Build OK!!!!!!!!
+echo ------------------------------
+
