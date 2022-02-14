afccaaef8ac926bf3d2ccf1462e977603379e342
Yomei Otani <yomei.otani@gmail.com>
2022-02-14 12:36:44 +0900

    Add files via upload

---
+++ server/scatter_plot/src/jq_bridge.h
@@ -0,0 +1,474 @@
+﻿#ifndef JQ_BRIDGE_H_
+#define JQ_BRIDGE_H_
+
+
+
+//
+// begin of jq_bridge
+//
+
+#ifdef JAVASCRIPT
+jqbGetText = function(sel)
+#else
+var jqbGetText(var sel)
+#endif
+{
+	var ret="";
+#ifdef JAVASCRIPT
+	ret=$(sel).text();
+#else
+	var str = "jqGetText(";
+	str += sel;
+	str += ")";
+	println(str);
+#endif
+	return ret;
+}
+
+#ifdef JAVASCRIPT
+jqbSetText = function(sel,src)
+#else
+void jqbSetText(var sel,var src)
+#endif
+{
+#ifdef JAVASCRIPT
+	$(sel).text(src);
+#else
+	var str = "jqSetText(";
+	str += sel;
+	str += ",";
+	str += src;
+	str += ")";
+	println(str);
+#endif
+}
+
+#ifdef JAVASCRIPT
+jqbGetHtml = function(sel)
+#else
+var jqbGetHtml(var sel)
+#endif
+{
+	var ret="";
+#ifdef JAVASCRIPT
+	ret = $(sel).html();
+#else
+	var str = "jqGetHtml(";
+	str += sel;
+	str += ")";
+	println(str);
+#endif
+	return ret;
+}
+
+#ifdef JAVASCRIPT
+jqbSetHtml = function(sel, src)
+#else
+void jqbSetHtml(var sel, var src)
+#endif
+{
+#ifdef JAVASCRIPT
+	$(sel).html(src);
+#else
+	var str = "jqSetHtml(";
+	str += sel;
+	str += ",";
+	str += src;
+	str += ")";
+	println(str);
+#endif
+}
+
+//
+// event
+//
+
+#ifdef JAVASCRIPT
+jqbCreateClickEvent = function(sel, func, param)
+#else
+void jqbCreateClickEvent(var sel, var func, var param)
+#endif
+{
+#ifdef JAVASCRIPT
+	$(sel).click(function(){
+		var p_ = param;
+		var f_ = func;
+		f_(p_);
+	});
+#else
+	var str = "jqCreateClickEvent(";
+	str += sel;
+	str += ",,";
+	str += param;
+	str += ")";
+	println(str);
+#endif
+}
+
+
+#ifdef JAVASCRIPT
+jqbCreateChangeEvent = function(sel, func, param)
+#else
+void jqbCreateChangeEvent(var sel, var func, var param)
+#endif
+{
+#ifdef JAVASCRIPT
+	$(sel).change(function(){
+		var p_ = param;
+		var f_ = func;
+		f_(p_);
+	});
+#else
+	var str = "jqCreateChangeEvent(";
+	str += sel;
+	str += ",,";
+	str += param;
+	str += ")";
+	println(str);
+#endif
+}
+
+
+//
+// setter getter
+//
+
+#ifdef JAVASCRIPT
+jqbSetAttr = function(sel,name,src)
+#else
+void jqbSetAttr(var sel,var name, var src)
+#endif
+{
+#ifdef JAVASCRIPT
+	$(sel).attr(name,src);
+#else
+	var str = "jqSetttr(";
+	str += sel;
+	str += ",";
+	str += name;
+	str += ",";
+	str += src;
+	str += ")";
+	println(str);
+#endif
+}
+
+
+#ifdef JAVASCRIPT
+jqbGetProp = function(sel, name)
+#else
+var jqbGetProp(var sel,var name)
+#endif
+{
+	var ret = "";
+#ifdef JAVASCRIPT
+	ret = $(sel).prop(name);
+#else
+	var str = "jqGetProp(";
+	str += sel;
+	str += ",";
+	str += name;
+	str += ")";
+	println(str);
+	ret = name;
+#endif
+	return ret;
+}
+
+#ifdef JAVASCRIPT
+jqbSetProp = function(sel, name, value)
+#else
+void jqbSetProp(var sel, var name, var value)
+#endif
+{
+#ifdef JAVASCRIPT
+	$(sel).prop(name, value);
+#else
+	var str = "jqSetProp(";
+	str += sel;
+	str += ",";
+	str += name;
+	str += ",";
+	str += value;
+	str += ")";
+	println(str);
+#endif
+}
+
+#ifdef JAVASCRIPT
+jqbGetVal = function(sel)
+#else
+var jqbGetVal(var sel)
+#endif
+{
+	var ret = "";
+#ifdef JAVASCRIPT
+	ret = $(sel).val();
+#else
+	var str = "jqGetval(";
+	str += sel;
+	str += ")";
+	println(str);
+#endif
+	return ret;
+}
+
+#ifdef JAVASCRIPT
+jqbSetVal = function(sel, value)
+#else
+void jqbSetVal(var sel, var value)
+#endif
+{
+#ifdef JAVASCRIPT
+	$(sel).val(value);
+#else
+	var str = "jqSetProp(";
+	str += sel;
+	str += ",";
+	str += value;
+	str += ")";
+	println(str);
+#endif
+}
+
+#ifdef JAVASCRIPT
+jqbSetCss = function(sel, name, value)
+#else
+void jqbSetCss(var sel, var name, var value)
+#endif
+{
+#ifdef JAVASCRIPT
+	$(sel).css(name, value);
+#else
+	var str = "jqSetCss(";
+	str += sel;
+	str += ",";
+	str += name;
+	str += ",";
+	str += value;
+	str += ")";
+	println(str);
+#endif
+}
+
+
+//
+// element
+//
+
+#ifdef JAVASCRIPT
+jqbGetElement = function(sel)
+#else
+var jqbGetElement(var sel)
+#endif
+{
+	var ret="";
+	ret = jqbGetProp(sel,"outerHTML");
+#ifndef JAVASCRIPT
+	ret = "<div id=\"tag1\"></div>";
+#endif
+	return ret;
+}
+
+#ifdef JAVASCRIPT
+jqbReplaceString = function(str,a,b)
+#else
+var jqbReplaceString(var str,var a,var b)
+#endif
+{
+	while (str.indexOf(a) != -1){
+		str = str.replace(a, "*");
+	}
+	while (str.indexOf("*") != -1){
+		str = str.replace("*", b);
+	}
+	return str;
+}
+
+#ifdef JAVASCRIPT
+jqbAppendString = function(sel,str)
+#else
+void jqbAppendString(var sel,var str)
+#endif
+{
+#ifdef JAVASCRIPT
+	ret = $(sel).parent().append(str);
+#else
+	var s = "jqAppendString(";
+	s += sel;
+	s += ",";
+	s += str;
+	s += ")";
+	println(s);
+#endif
+}
+
+
+
+#ifdef JAVASCRIPT
+jqbCopyElement = function(sel,a,b)
+#else
+void jqbCopyElement(var sel,var a,var b)
+#endif
+{
+	var str;
+	str = jqbGetElement(sel);
+	str = jqbReplaceString(str,a, b);
+	jqbAppendString(sel, str);
+}
+
+
+
+//
+// Input
+//
+
+
+
+#ifdef JAVASCRIPT
+jqbSetInputRadioVal = function(sel, value)
+#else
+void jqbSetInputRadioVal(var sel, var value)
+#endif
+{
+	var str;
+	str = "input[name=" + sel;
+	str += "][value=" + value;
+	str+="]";
+	jqbSetProp(str,"checked", true);
+}
+
+#ifdef JAVASCRIPT
+jqbGetInputRadioVal = function(sel)
+#else
+var jqbGetInputRadioVal(var sel)
+#endif
+{
+	var ret=NaN;
+	var str;
+
+	str = "input[name=" + sel;
+	str += "]:checked";
+	ret=jqbGetVal(str);
+	if (typeof(ret) == "undefined")ret = "";
+	return ret;
+}
+
+#ifdef JAVASCRIPT
+jqbSetInputCheckVal = function(sel, value)
+#else
+void jqbSetInputCheckVal(var sel, var value)
+#endif
+{
+	var str;
+	str = "input[name=" + sel;
+	str += "]";
+	jqbSetProp(str, "checked", value);
+}
+
+#ifdef JAVASCRIPT
+jqbGetInputCheckVal = function(sel)
+#else
+var jqbGetInputCheckVal(var sel)
+#endif
+{
+	var ret = false;
+	var str;
+
+	str = "input[name=" + sel;
+	str += "]";
+	ret = jqbGetProp(str,"checked");
+	return ret;
+}
+
+//
+// Media
+//
+
+
+
+#ifdef JAVASCRIPT
+jqbMediaInit = function(sel, value)
+#else
+void jqbMediaInit(var sel, var value)
+#endif
+{
+#ifdef JAVASCRIPT
+	var s=$(sel);
+	try{
+		s[0].pause();
+	}catch (e){}
+	try{
+		s[0].preload = "none";
+	}catch (e){}
+	try{
+		s[0].src = value;
+	}catch (e){}
+	try{
+		s[0].currentTime = 0;
+	}
+	catch (e){}
+#else
+	var str;
+	str = "jqbMediaInit(";
+	str += sel;
+	str += ",";
+	str += value;
+	str += ")";
+	println(str);
+#endif
+}
+
+#ifdef JAVASCRIPT
+jqbMediaStop = function(sel)
+#else
+void jqbMediaStop(var sel)
+#endif
+{
+#ifdef JAVASCRIPT
+	var s = $(sel);
+	try{
+		s[0].pause();
+	}catch (e){}
+#else
+	var str;
+	str = "jqbMediaPause(";
+	str += sel;
+	str += ")";
+	println(str);
+#endif
+}
+
+#ifdef JAVASCRIPT
+jqbMediaPlay = function(sel)
+#else
+void jqbMediaPlay(var sel)
+#endif
+{
+#ifdef JAVASCRIPT
+	var s = $(sel);
+	try{
+		s[0].pause();
+	}catch (e){}
+	try{
+		s[0].currentTime=0;
+	}catch (e){}
+	try{
+		s[0].play();
+	}catch (e){}
+#else
+	var str;
+	str = "jqbMediaPlay(";
+	str += sel;
+	str += ")";
+	println(str);
+#endif
+}
+
+
+//
+// end of jq_bridge
+//
+
+#endif /*  jq_bridge.h */
+
---
+++ server/scatter_plot/src/js_query.h
@@ -0,0 +1,425 @@
+﻿#ifndef JS_QUERY_H_
+#define JS_QUERY_H_
+
+//
+//begin of js_query
+//
+
+
+#ifndef JAVASCRIPT
+#define null 0
+#define true 1
+#define false 0
+#endif
+
+#ifdef JAVASCRIPT 
+#define BEGIN_TRY() try{
+#define END_TRY() }catch(e){}
+#else
+#define BEGIN_TRY()
+#define END_TRY()
+#endif
+
+#ifdef JAVASCRIPT 
+#define _delete(a,b) delete a[b]
+#else
+#define _delete(a,b) a.delete_(b)
+#endif
+
+//
+//
+//
+
+#ifndef JAVASCRIPT 
+#define __QUERY__ "?i=iii&p=ppp&s=1&=000"
+//#define __QUERY__ null
+#endif
+//
+// Debufg print
+//
+
+#ifdef JAVASCRIPT
+println = function(src)
+#else
+void println(var src)
+#endif
+{
+#ifdef JAVASCRIPT
+	alert(src);
+#else
+	printf("%s\n", ((std::string)src).c_str());
+#endif
+}
+
+#ifndef JAVASCRIPT
+void println(const char* a)
+{
+	var aa;
+	aa = a;
+	println(aa);
+}
+#endif
+
+//
+// length
+//
+
+
+#ifdef JAVASCRIPT
+strlength = function(src)
+#else
+var strlength(var src)
+#endif
+{
+	var ret = 0;
+	if (src == null || src == "")return ret;
+#ifdef JAVASCRIPT
+	ret = src.length;
+#else
+	ret = src.length();
+#endif
+	return ret;
+}
+
+#ifndef JAVASCRIPT
+void strlength(const char* a)
+{
+	var aa;
+	aa = a;
+	strlength(aa);
+}
+#endif
+
+
+//
+// QUERY
+//
+
+
+#ifdef JAVASCRIPT
+setQuery = function(src)
+#else
+void setQuery(var src)
+#endif
+{
+	if (src == null || src.indexOf("?") != 0)return;
+	if (typeof(sessionStorage) != "undefined"){
+#ifdef JAVASCRIPT
+		// support for safari private mode
+		try{
+#endif
+			sessionStorage.setItem("__QUERY__", src);
+			return;
+#ifdef JAVASCRIPT
+		}catch (e){}
+#endif
+	}
+#ifdef JAVASCRIPT
+	__QUERY__ = src;
+#endif
+}
+
+#ifdef JAVASCRIPT
+clearQuery = function()
+#else
+void clearQuery()
+#endif
+{
+	if (typeof(sessionStorage) != "undefined"){
+#ifdef JAVASCRIPT
+		// support for safari private mode
+		try{
+#endif
+			sessionStorage.removeItem("__QUERY__");
+			return;
+#ifdef JAVASCRIPT
+		}catch (e){}
+#endif
+	}
+#ifdef JAVASCRIPT
+	__QUERY__ = null;
+#endif
+}
+
+var s_query = null;
+
+#ifdef JAVASCRIPT
+getQuery = function()
+#else
+var getQuery()
+#endif
+{
+	var ret;
+	ret = "";
+#ifdef JAVASCRIPT
+	if (location.search){
+		ret = location.search;
+		return ret;
+	}
+#endif
+	if (typeof(sessionStorage) != "undefined"){
+		if (s_query == null){
+			ret = sessionStorage.getItem("__QUERY__");
+			clearQuery();
+			s_query = ret;
+		}
+		else{
+			ret = s_query;
+		}
+		if (ret != null)return ret;
+	}
+#ifndef JAVASCRIPT
+#ifdef __QUERY__
+	ret = __QUERY__;
+#endif
+#endif
+	return ret;
+}
+
+//
+// location
+//
+
+
+#ifdef JAVASCRIPT
+jumpLocation = function(src)
+#else
+void jumpLocation(var src)
+#endif
+{
+	if (src == null)return;
+#ifdef JAVASCRIPT
+	if (typeof(__QUERY__) != "undefined")src += __QUERY__;
+	location.href = src;
+#else
+	println("jumpLocation()");
+	println(src);
+#endif
+}
+
+
+#ifdef JAVASCRIPT
+myName = function()
+#else
+var myName()
+#endif
+{
+	var ret = "";
+	var idx = -1;
+
+#ifdef JAVASCRIPT
+	ret = location.href;
+#else
+	ret = __FILE__;
+#endif
+	idx = ret.lastIndexOf("/");
+	if (idx != -1)ret = ret.substring(idx + 1);
+	idx = ret.lastIndexOf("\\");
+	if (idx != -1)ret = ret.substring(idx + 1);
+	idx = ret.lastIndexOf("?");
+	if (idx != -1)ret = ret.substring(0, idx);
+	return ret;
+}
+
+
+//
+// query value
+//
+
+#ifdef JAVASCRIPT
+getQueryValue = function(src, key)
+#else
+var getQueryValue(var src, var key)
+#endif
+{
+	var ret = "";
+	var idx;
+	if (src == null || src == "")return ret;
+	if (key == null || key == "")return ret;
+	do{
+		idx = src.indexOf("+");
+		if (idx == -1)break;
+		src = src.replace("+", "%20");
+	} while (true);
+	key = key + "=";
+	idx = src.indexOf(key);
+	if (idx == -1)return ret;
+	ret = src.substring(idx + strlength(key));
+	idx = ret.indexOf("&");
+	if (idx != -1)ret = ret.substring(0, idx);
+	ret = decodeURIComponent(ret);
+	return ret;
+}
+
+#ifdef JAVASCRIPT
+createQueryValue = function(key, val)
+#else
+var createQueryValue(var key, var val)
+#endif
+{
+	var ret = "";
+	if (key == null || key == "")return ret;
+	if (val == null || val == "")key = "";
+	ret = "?";
+	ret += key + "=";
+	ret += encodeURIComponent(val);
+	return ret;
+}
+
+#ifdef JAVASCRIPT
+appendQueryValue = function(key, val)
+#else
+var appendQueryValue(var key, var val)
+#endif
+{
+	var ret = "";
+	if (key == null || key == "")return ret;
+	if (val == null || val == "")key = "";
+	ret = "&";
+	ret += key + "=";
+	ret += encodeURIComponent(val);
+	return ret;
+}
+
+//
+// query debug
+//
+
+#ifdef JAVASCRIPT
+getQueryLocationSearch = function()
+#else
+var getQueryLocationSearch()
+#endif
+{
+	var ret = null;
+#ifdef JAVASCRIPT
+	if (location.search){
+		ret = location.search;
+	}
+#endif
+	return ret;
+}
+
+#ifdef JAVASCRIPT
+getQuerySessionStorage = function()
+#else
+var getQuerySessionStorage()
+#endif
+{
+	var ret = null;
+	if (typeof(sessionStorage) != "undefined"){
+		ret = sessionStorage.getItem("__QUERY__");
+	}
+	return ret;
+}
+
+//
+// print,mail,open new window 
+//
+
+
+#ifdef JAVASCRIPT
+printPage = function()
+#else
+void printPage()
+#endif
+{
+	BEGIN_TRY();
+#ifdef JAVASCRIPT
+	print();
+#else
+	println("printPage()");
+#endif
+	END_TRY();
+}
+
+#ifdef JAVASCRIPT
+openPage = function(src)
+#else
+void openPage(var src)
+#endif
+{
+	// This function should be called from onClick events
+	BEGIN_TRY();
+#ifdef JAVASCRIPT
+	window.open(src);
+#else
+	println("openPage()");
+	println(src);
+#endif
+	END_TRY();
+}
+
+#ifdef JAVASCRIPT
+mailTo = function(addr, subject, body)
+#else
+void mailTo(var addr, var subject, var body)
+#endif
+{
+	BEGIN_TRY();
+#ifdef JAVASCRIPT
+	location.href = 'mailto:' + addr + '?subject=' + subject + '&body=' + body;
+#else
+	var str;
+	println("mailTo()");
+	str = "address:";
+	str += addr;
+	println(str);
+	str = "subject:";
+	str += subject;
+	println(str);
+	str = "body:";
+	str += body;
+	println(str);
+#endif
+	END_TRY();
+}
+
+
+#ifdef JAVASCRIPT
+jumpLocation2 = function(src)
+#else
+void jumpLocation2(var src)
+#endif
+{
+	if (src != myName()){
+		jumpLocation(src);
+		return;
+	}
+#ifdef JAVASCRIPT
+	var s,p,u,q;
+	var li={};
+	s_query=null;
+	q=getQuery();
+	for(s in include_list){
+		li[s] = 1;
+	}
+	for (s in li){
+		p=s.indexOf("?");
+		if (p == -1) {
+			p = s.indexOf(".exe");
+			if (p == -1)continue;
+			p += 4;
+		}
+		u=s.substr(0, p);
+		u += q;
+
+		delete include_list[s];
+		include(u);
+	}
+	try{
+		onJumpLocation2();
+	}catch (e){}
+#else
+	println("jumpLocation2()");
+	println(src);
+#endif
+}
+
+
+//
+// end of js_query
+//
+
+#endif /*  JS_QUERY_H_ */
+
